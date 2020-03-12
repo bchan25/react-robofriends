a075c615fcc35371f427594d6168c180611d8ee8
Ben Chan <ben@chanhq.com>
2020-03-12 15:56:24 +0000

    Created Card Component with props

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component{
+const Card = ({id, name, email}) => {
 
-    render(){
-        return <h1>Card</h1>
-    }
+   return (
+       <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+           <img alt='robots' src={`https://robohash.org/${id}?200x200'/`} />
+           <div>
+               <h2>{name}</h2>
+               <p>{email}</p>
+           </div>
+       </div>
+   )
+    
 }
 
 export default Card;
