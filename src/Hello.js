cbfffd449cb27d3c6c349a84153690261cec0944
Ben Chan <ben@chanhq.com>
2020-03-12 15:30:57 +0000

    Coverted Hello component to a function

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hello.css';
 
-class Hello extends Component{
+function Hello (props){
 
-    render(){
-        return (
-            <div className="f1 tc">
-                <h1>Hello World</h1>
-                <p>{this.props.greeting}</p>
-            </div>
-        )   
-    }
+    return (
+        <div className="f1 tc">
+            <h1>Hello World</h1>
+            <p>{props.greeting}</p>
+        </div>
+    )   
 }
 
 export default Hello;
